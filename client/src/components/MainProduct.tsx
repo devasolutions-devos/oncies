8572294b3cccaf9ebce821e721f00432b519138e
devasolutions-devos <4tysdead@gmail.com>
2026-07-10 05:38:43 -0600

    considering refactoring for client. using next.js next time

--- client/src/components/MainProduct.tsx
+++ client/src/components/MainProduct.tsx
@@ -2,7 +2,7 @@ type MainProductProps = {
     product_id: number;
 }
 
-function MainProduct({product_id}: MainProductProps) {
+async function MainProduct({product_id}: MainProductProps) {
 
     function onClick(e: React.MouseEvent) {
         e.preventDefault();
@@ -10,7 +10,15 @@ function MainProduct({product_id}: MainProductProps) {
     }
 
     //FETCH from
-    
+    const response = await fetch("http://127.0.0.1:3000/products")
+
+    if(!response.ok) {
+        throw new Error(`${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
     return (
         <div>
             <img src="https://placehold.co/500x500/png"/>
