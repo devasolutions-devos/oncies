b63720929e7203d69bc2a8c00f6a8b99a445832f
devasolutions-devos <4tysdead@gmail.com>
2026-07-10 05:38:53 -0600

    considering refactoring for client. using next.js next time

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,12 @@ import express from "express";
 import productRoutes from "./routes/product.route.ts";
 import cartRoutes from "./routes/cart.route.ts"
 import cartItemRoutes from "./routes/cart_item.route.ts";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/products", productRoutes);
 app.use("/carts", cartRoutes);
 app.use("/cart_item", cartItemRoutes);
